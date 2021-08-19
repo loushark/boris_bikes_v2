ea0de6c07e51aeeb1770cfe73bb0081d089a30f8
Lou Reade <39278690+loushark@users.noreply.github.com>
2021-08-19 15:40:52 +0100

    no more bikes can be added to a dock at capacity

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -2,16 +2,22 @@ require 'Bike'
 
 class DockingStation
 
+  def initialize
+    @bikes = []
+    @capacity = 20
+  end
+
   def dock_bike(bike)
-    @bike = bike
+    raise "Dock is at capacity. No more bikes can be docked" if @bikes.length == @capacity
+    @bikes << bike
   end
 
   def release_bike
     raise "This bike is broken" if !is_bike_working?
-    @bike
+    @bikes[0]
   end
 
   def is_bike_working?
-   @bike.working?
+   @bikes[0].working?
  end
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -4,29 +4,38 @@ describe DockingStation do
   let(:test_dock1) { DockingStation.new }
   let(:test_bike1) { instance_double "Bike" }
 
-  before do
-    test_dock1.dock_bike(test_bike1)
+  context 'when docking a bike' do
+    it 'can dock a bike' do
+      # expect(test_dock1).to respond_to :dock_bike
+      expect(test_dock1.dock_bike(test_bike1)).to eq [test_bike1]
+    end
+
+    it 'can dock up to a capacity number of bikes' do
+      20.times { test_dock1.dock_bike(test_bike1) }
+      expect { test_dock1.dock_bike(test_bike1) }.to raise_error "Dock is at capacity. No more bikes can be docked"
+    end
   end
 
-  it 'can dock a bike' do
-    expect(test_dock1).to respond_to :dock_bike
-    expect(test_dock1.dock_bike(test_bike1)).to eq test_bike1
-  end
+  context 'when releasing a bike' do
+    before do
+      test_dock1.dock_bike(test_bike1)
+    end
 
-  it 'can release a bike' do
-    allow(test_bike1).to receive(:working?).and_return true
-    expect(test_dock1).to respond_to :release_bike
-    expect(test_dock1.release_bike).to eq test_bike1
+    it 'can release a bike' do
+      allow(test_bike1).to receive(:working?).and_return true
+      expect(test_dock1).to respond_to :release_bike
+      expect(test_dock1.release_bike).to eq test_bike1
+    end
+
+    it 'cannot release a bike that is broken' do
+      allow(test_bike1).to receive(:working?).and_return false
+      expect { test_dock1.release_bike }.to raise_error "This bike is broken"
+    end
   end
 
   it 'can check if a bike is working' do
+    test_dock1.dock_bike(test_bike1)
     allow(test_bike1).to receive(:working?).and_return true
     expect(test_dock1.is_bike_working?).to eq true
   end
-
-  it 'cannot release a bike that is broken' do
-    allow(test_bike1).to receive(:working?).and_return false
-    expect { test_dock1.release_bike }.to raise_error "This bike is broken"
-  end
-
 end
