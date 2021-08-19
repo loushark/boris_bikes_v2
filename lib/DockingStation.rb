4abaae5d5a2aaeb039912974de6b744422397e3e
Lou Reade <39278690+loushark@users.noreply.github.com>
2021-08-19 14:36:11 +0100

    a Bike can be released from DockingStation

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,6 +1,10 @@
 class DockingStation
 
   def dock_bike(bike)
-    bike
+    @bike = bike
+  end
+
+  def release_bike
+    @bike
   end
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -8,4 +8,10 @@ describe DockingStation do
     expect(test_dock1).to respond_to :dock_bike
     expect(test_dock1.dock_bike(test_bike1)).to eq test_bike1
   end
+
+  it 'can release a bike' do
+    test_dock1.dock_bike(test_bike1)
+    expect(test_dock1).to respond_to :release_bike
+    expect(test_dock1.release_bike).to eq test_bike1
+  end
 end
